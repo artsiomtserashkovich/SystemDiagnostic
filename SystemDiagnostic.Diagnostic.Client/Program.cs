b53a3a5addb3cf85a7bbb14f14bca1cf71d898ca
ArtemTereshkovich <artyomt7777@gmail.com>
2018-04-16 21:27:47 +0300

    init commit

---
+++ SystemDiagnostic.Diagnostic.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SystemDiagnostic.Diagnostic.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
